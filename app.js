109ca7bda11d4d24e9ffec56fdb05f5bcf4dfda7
dcristobal-sf <44408128+dcristobal-sf@users.noreply.github.com>
2023-02-14 17:23:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var nforce = require('nforce');
 var express = require('express');
 var port = process.env.PORT || 3000;
-var cors = require('cors');
+const cors = require('cors');
 
 var org = nforce.createConnection({
   clientId: process.env.CONSUMER_KEY,
